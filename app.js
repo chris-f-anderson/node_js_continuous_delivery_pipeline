74dc9a9f5441733d004d2e75a532e5927df8c2b3
Your Name <you@example.com>
2024-06-10 14:59:00 -0500

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('How To Create a Continuous Delivery Pipeline to demonstrate DevOps'));
+app.get('/', (req, res) => res.send('How To Create a Continuous Delivery Pipeline to demonstrate DevOps using AWS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
